b69ab5a9d61168e69a992cccbdb74f542830fb76
rboudrouss <r.boudrouss@gmail.com>
2022-08-08 00:15:53 +0200

    logic dropdown working

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import './App.css';
+import styles from './App.css';
+import Header from './components/Header/Header';
+import Dropdown from './components/Dropdown/Dropdown';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.main}>
+      <Header />
+      <Dropdown
+        title="ceci est un titre"
+        text="le text uwu owo lorem ipsum mes couilles tout ça"
+        children={[
+          { title: "nyah", text: "lorem tout ça couilles" },
+          {
+            title: "nyah", text: "lorem tout ça couilles", children:[
+              { title: "nyah", text: "lorem tout ça couilles" },
+              { title: "nyah", text: "lorem tout ça couilles" },
+            ]
+          },
+        ]}
+      />
       <p>uwu</p>
     </div>
   );
---
+++ src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,25 @@
+import styles from "./Dropdown.module.css";
+import React, { useState } from 'react';
+
+let Dropdown = ({ title, text, opened, children }) => {
+    const [open, setOpen] = useState(opened ?? false);
+
+    let handleClick = () => {
+        setOpen(!open);
+    }
+
+    let dropdowns = children && children.map(child => Dropdown(child));
+
+    return <div className={styles.container}>
+        <button className={styles.button} onClick={handleClick}>
+            <p className={styles.title}>
+                {title}
+            </p>
+        </button>
+        {open && (<p className={styles.text}> {text}</p>)}
+        {open && dropdowns}
+    </div>;
+
+}
+
+export default Dropdown;
