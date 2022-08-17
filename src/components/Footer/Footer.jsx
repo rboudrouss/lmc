19ff719e8ac43fe0848fe820a4bfca6b3ee1954f
rboudrouss <r.boudrouss@gmail.com>
2022-08-17 14:00:23 +0200

    corrected warnings

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,27 +4,27 @@ let Footer = () => <footer className={styles.container}>
     <a href="mailto:lamatricecarree@gmail.com" className={styles.link}>lamatricecarree@gmail.com</a>
     <ul className={styles.social_list}>
         <li className={styles.social_list__item}>
-            <a className={styles.social_list__link} href="https://discord.gg/7yNYMsQj5n" target="_blank">
+            <a className={styles.social_list__link} href="https://discord.gg/7yNYMsQj5n" target="_blank" rel="noreferrer">
                 <i className="fab fa-discord"></i>
             </a>
         </li>
         <li className={styles.social_list__item}>
-            <a className={styles.social_list__link} href="https://www.youtube.com/channel/UCIDq2rOKER9TbY8YfPq8-KA" target="_blank">
+            <a className={styles.social_list__link} href="https://www.youtube.com/channel/UCIDq2rOKER9TbY8YfPq8-KA" target="_blank" rel="noreferrer">
                 <i className="fab fa-youtube"></i>
             </a>
         </li>
         <li className={styles.social_list__item}>
-            <a className={styles.social_list__link} href="https://www.instagram.com/LaMatriceCarree/" target="_blank">
+            <a className={styles.social_list__link} href="https://www.instagram.com/LaMatriceCarree/" target="_blank" rel="noreferrer">
                 <i className="fab fa-instagram"></i>
             </a>
         </li>
         <li className={styles.social_list__item}>
-            <a className={styles.social_list__link} href="https://www.facebook.com/LaMatriceCarree/" target="_blank">
+            <a className={styles.social_list__link} href="https://www.facebook.com/LaMatriceCarree/" target="_blank" rel="noreferrer">
                 <i className="fab fa-facebook"></i>
             </a>
         </li>
         <li className={styles.social_list__item}>
-            <a className={styles.social_list__link} href="https://twitter.com/LaMatriceCarree" target="_blank">
+            <a className={styles.social_list__link} href="https://twitter.com/LaMatriceCarree" target="_blank" rel="noreferrer">
                 <i className="fab fa-twitter"></i>
             </a>
         </li>
