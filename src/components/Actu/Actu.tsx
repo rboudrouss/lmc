2bdd090d5f90f4470f161df3f5f19572f945d658
rboudrouss <r.boudrouss@gmail.com>
2023-10-21 23:18:17 +0200

    fixed favicon

--- src/components/Actu/Actu.tsx
+++ src/components/Actu/Actu.tsx
@@ -13,7 +13,9 @@ export default function Actu(props: {
       <div className={styles.main_content}>
         <h1>{props.title}</h1>
         <p>{props.content}</p>
-        {props.img && <img className={styles.img} src={props.img} alt={props.title} />}
+        {props.img && (
+          <img className={styles.img} src={props.img} alt={props.title} />
+        )}
       </div>
       <div className={styles.add_info}>
         <p>date {props.date.toDateString()}</p>
