1ad598987eef6f9d8babe72964f3cd931f65ebd0
rboudrouss <r.boudrouss@gmail.com>
2023-11-10 23:25:47 +0100

    tags and text is working

--- src/components/Actu/Actu.tsx
+++ src/components/Actu/Actu.tsx
@@ -4,14 +4,13 @@ import { assosToImg } from "../../helpers";
 
 export default function Actu(props: {
   title: string;
-  content?: any;
+  content?: string;
   date?: Date;
   img?: string;
   source?: string;
   assos: string[];
 }) {
   let { title, content, date, img, source, assos } = props;
-  content = <content />;
 
   return (
     <div className={styles.wrapper}>
@@ -19,21 +18,27 @@ export default function Actu(props: {
         <h3 className={styles.date}>{date?.toDateString()}</h3>
         <div className={styles.header}>
           <div className={styles.assos}>
-            {assos.map((asso) => (
-              assosToImg(assos) && <img
-                src={assosToImg(asso)}
-                alt={asso}
-                className={styles.asso}
-                key={asso}
-              />
-            ))}
+            {assos.map(
+              (asso) =>
+                assosToImg(asso) && (
+                  <img
+                    src={assosToImg(asso)}
+                    alt={asso}
+                    className={styles.asso}
+                    key={asso}
+                  />
+                )
+            )}
           </div>
           <h3 className={styles.title}>{title}</h3>
         </div>
         <div className={styles.body}>
           {content && (
             <div className={styles.textWrapper}>
-              <p className={styles.text}>{<content />}</p>
+              <p
+                className={styles.text}
+                dangerouslySetInnerHTML={{ __html: content }} // FIXME find a better way
+              ></p>
             </div>
           )}
           {img && <img src={img} alt="" className={styles.img} />}
