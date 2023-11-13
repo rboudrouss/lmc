23f57da1c1fc82e3d7318e79796be440f3cb49d3
rboudrouss <r.boudrouss@gmail.com>
2023-11-13 13:01:21 +0100

    selector working but still ugly

--- src/components/ActuPage/ActuPage.tsx
+++ src/components/ActuPage/ActuPage.tsx
@@ -28,14 +28,19 @@ export default function ActuPage(props: { actus?: ActuT[] }) {
 // TODO
 function filter(selection: SelectionT, actus: ActuT[]): ActuT[] {
   return actus.filter((actu) => {
-    if (selection.persistant && !actu.persistant) return false;
+    if (!(selection.event && selection.persistant)) {
+      if (selection.event && actu.persistant) return false;
+      if (selection.persistant && !actu.persistant) return false;
+    }
+
     if (selection.date_start && actu.date < selection.date_start) return false;
     if (selection.date_end && actu.date > selection.date_end) return false;
     if (selection.tags) {
       for (let tag of selection.tags) {
+        console.log("tag", tag, actu.assos);
         if (!actu.assos.includes(tag)) return false;
       }
     }
     return true;
-  })
+  });
 }
--- src/components/Selector/Selector.tsx
+++ src/components/Selector/Selector.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export interface SelectionT {
   tags?: string[];
+  event?: boolean;
   persistant?: boolean;
   date_start?: Date;
   date_end?: Date;
@@ -11,48 +12,86 @@ export default function Selector(props: {
   onChange: (selection: SelectionT) => void;
 }) {
   const [selection, setSelection] = useState<SelectionT>({});
-  console.log("uwu",selection);
 
-  return <div>
+  const onChange = (selection: SelectionT) => {
+    setSelection(selection);
+    props.onChange(selection);
+  };
+
+  return (
     <div>
-      <input type="checkbox" name="persistant" id="persistant" onChange={(e) => {
-        setSelection({
-          ...selection,
-          persistant: e.target.checked,
-        });
-        props.onChange(selection);
-      }} />
-      <label htmlFor="persistant">Persistant</label>
+      <div>
+        <input
+          type="checkbox"
+          name="event"
+          id="event"
+          defaultValue="on"
+          onChange={(e) =>
+            onChange({
+              ...selection,
+              event: e.target.checked,
+            })
+          }
+        />
+        <label htmlFor="persistant">Event</label>
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          name="persistant"
+          id="persistant"
+          defaultValue="on"
+          onChange={(e) =>
+            onChange({
+              ...selection,
+              persistant: e.target.checked,
+            })
+          }
+        />
+        <label htmlFor="persistant">Persistant</label>
+      </div>
+      <div>
+        <input
+          type="date"
+          name="date_start"
+          id="date_start"
+          onChange={(e) =>
+            onChange({
+              ...selection,
+              date_start: new Date(e.target.value),
+            })
+          }
+        />
+        <label htmlFor="date_start">Date de début</label>
+      </div>
+      <div>
+        <input
+          type="date"
+          name="date_end"
+          id="date_end"
+          onChange={(e) =>
+            onChange({
+              ...selection,
+              date_end: new Date(e.target.value),
+            })
+          }
+        />
+        <label htmlFor="date_end">Date de fin</label>
+      </div>
+      <div>
+        <input
+          type="text"
+          name="tags"
+          id="tags"
+          onChange={(e) =>
+            onChange({
+              ...selection,
+              tags: e.target.value.trim().toLocaleLowerCase().split(" "),
+            })
+          }
+        />
+        <label htmlFor="tags">Associations</label>
+      </div>
     </div>
-    <div>
-      <input type="date" name="date_start" id="date_start" onChange={(e) => {
-        setSelection({
-          ...selection,
-          date_start: new Date(e.target.value),
-        });
-        props.onChange(selection);
-      }} />
-      <label htmlFor="date_start">Date de début</label>
-    </div>
-    <div>
-      <input type="date" name="date_end" id="date_end" onChange={(e) => {
-        setSelection({
-          ...selection,
-          date_end: new Date(e.target.value),
-        });
-        props.onChange(selection);
-      }} />
-      <label htmlFor="date_end">Date de fin</label>
-    </div>
-    <div>
-      <input type="text" name="tags" id="tags" onChange={(e) => {
-        setSelection({
-          ...selection,
-          tags: e.target.value.split(" "),
-        });
-        props.onChange(selection);
-      }} />
-      <label htmlFor="tags">Tags</label>
-    </div>
-  </div>;
+  );
 }
