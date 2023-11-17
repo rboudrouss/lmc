c082b57652b557e74890c43530f68a5d6ef34fd6
rboudrouss <r.boudrouss@gmail.com>
2023-11-17 01:05:13 +0100

    moved to a full static version of website

--- src/helpers/convert.cjs
+++ src/helpers/convert.cjs
@@ -1,9 +1,10 @@
+// Don't forget to remove "./00-La_Matrice_Carre" in the root links
 const drop_data_T = require("../data/dropdowns.json");
 const drop_data = drop_data_T;
 
 const fs = require("fs");
 
-function convertJsonMd(json, path, i) {
+function convertJsonMd(json, path, i, depth = 0) {
   if (!fs.existsSync(path)) fs.mkdirSync(path);
 
   const title = `${(i ? i.toString().padStart(2, "0") : "00") + "-"}${json.title
@@ -15,33 +16,54 @@ function convertJsonMd(json, path, i) {
 
   if (!fs.existsSync(folder)) fs.mkdirSync(folder);
 
-  let text = `---\ntitle: ${json.title}`;
+  let text = `---\nlayout: ../${
+    depth === 0
+      ? ""
+      : Array.from({ length: depth })
+          .map(() => "..")
+          .join("/")
+          .concat("/")
+  }layouts/Dropdowns.astro\ntitle: ${json.title}`;
 
-  if (json.isPage) {
-    text = text.concat(`\npage: true`);
+  console.log("text", text);
+
+  if (json.childs?.length > 0) {
+    text = text.concat("\nchilds:");
+    json.childs.forEach((child, i) => {
+      text = text.concat(
+        `\n  - title: "${child.title}"\n    href: "/${path.slice(
+          2 + 20 // HACK remove "./00-La_Matrice_Carre"
+        )}${path.length > 23 ? "/" : ""}${title}/${i
+          .toString()
+          .padStart(2, "0")}-${child.title
+          .match(/[a-zA-Z\s]/g)
+          .join("")
+          .replaceAll(" ", "_")}"`
+      );
+    });
   }
-  if (json.opened) {
-    text = text.concat(`\nopened: true`);
-  }
+
   text = text.concat(`\n---\n${json.content ?? ""}`);
 
-  const file = folder.concat("/").concat(title).concat(".md");
+  const file = folder.concat("/index.mdx");
 
-  console.log(`Creating file ${file}`);
+  console.log(`Creating file ${title}/index.mdx`);
+  console.log("path", path);
+  console.log("depth", depth);
 
   fs.writeFileSync(file, text);
 
   if (json.childs) {
     json.childs.forEach((child, i) => {
-      convertJsonMd(child, folder, i);
+      convertJsonMd(child, folder, i, depth + 1);
     });
   }
 }
 
 function commandJsonMd() {
   const json = drop_data;
-  const path = "./test";
-  convertJsonMd(json, path);
+  const path = ".";
+  convertJsonMd(json, path, 0);
 }
 
 commandJsonMd();
