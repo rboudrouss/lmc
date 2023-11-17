6b059afeb8f95aef769a12976582b576278d1ecd
rboudrouss <r.boudrouss@gmail.com>
2023-11-17 02:46:41 +0100

    added site in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()]
+  site: "https://lamatricecarre.com",
+  integrations: [react(), mdx()],
 });
