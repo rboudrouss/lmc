c9b92a85bbd1167e5858f889bb47a16bc189d578
rboudrouss <r.boudrouss@gmail.com>
2023-11-20 01:08:05 +0100

    don't need that anymore

--- src/components/actus/Header/Header.tsx
+++ src/components/actus/Header/Header.tsx
@@ -1,53 +1,25 @@
-import { useEffect, useState } from "react";
 import styles from "./Header.module.css";
 
-export default function Header(props: {
-  title: string;
-  goBack?: () => void;
-  goHome?: () => void;
-  arrow?: boolean;
-  show?: boolean;
-}) {
-  const { title, arrow, goBack, goHome, show } = props;
+import Bars from "@/icons/bars.svg";
 
-  const [showArrow, setShowArrow] = useState(false);
-
-  useEffect(() => {
-    setShowArrow(show);
-  }, [show]);
+export default function Header(props: { title: string; goBack?: () => void }) {
+  const { title, goBack } = props;
 
   return (
     <header className={styles.wrapper}>
-      {goHome ? (
-        <img
-          src="/assets/logo/logo.png"
-          alt="logo"
-          className={styles.logo}
-          onClick={() => {
-            goHome();
-          }}
-        />
-      ) : (
-        <a href="/">
-          <img src="/assets/logo/logo.png" alt="logo" className={styles.logo} />
-        </a>
-      )}
+      <a href="/">
+        <img src="/assets/logo/logo.png" alt="logo" className={styles.logo} />
+      </a>
 
       <h1 className={styles.title}>{title}</h1>
 
-      {arrow ? (
-        <i
-          className={`fa-solid fa-arrow-left fa-3x ${styles.arrow} `}
-          style={showArrow ? { cursor: "pointer", opacity: 1 } : { opacity: 0 }}
-          onClick={() => goBack()}
-        ></i>
-      ) : (
-        <i
-          className={`fa-solid fa-bars fa-3x ${styles.arrow}`} //FIXME très moche
-          style={{ cursor: "pointer" }}
-          onClick={() => goBack()}
-        ></i>
-      )}
+      <img
+        className={styles.bars}
+        src={Bars.src}
+        alt="menu"
+        style={{ cursor: "pointer" }}
+        onClick={() => goBack()}
+      />
     </header>
   );
 }
