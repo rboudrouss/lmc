0c8d67a585b718e7602ba9c18425aba85f9a72d0
rboudrouss <r.boudrouss@gmail.com>
2024-02-16 03:12:35 +0100

    added working parser

---
+++ src/pages/associations/parser.py
@@ -0,0 +1,39 @@
+# parser convertie la syntaxe rotor en syntaxe valide md
+
+import os
+import json
+
+files = os.listdir(".")
+mds = list(filter(lambda x : x.endswith("md") and not x.startswith("index"), files)) # on garde que les fichiers du dossier qui sont des .md
+print("uwu")
+for md in mds:
+  print(f"treating {md}")
+
+  content = filter(None, open(md).read().split("\n")) # divisent par saut de ligne, et on retire les lignes vide (quand 2 sauts de ligne)
+
+  content:list[str] = list(map(lambda x:x.strip().replace("\"", ""), content)) # les guillemets peuvent me gêner, je les retire
+
+  outJson: dict[str, str] = dict()
+  lastKey = ""
+
+  for line in content:
+    if ":" in line:
+      key,*value = map(lambda x:x.strip(), line.split(":"))
+      value = ":".join(value)
+      if key in outJson:
+        print(f"<!> warning key {key} already in dict {outJson}")
+      lastKey = key
+      outJson[key] = value
+    else:
+      outJson[lastKey] += "\n"
+      outJson[lastKey] += line
+    
+  
+  lines = map(lambda x:f"{x} : \"{outJson[x]}\"\n", outJson)
+  out = "---\n"
+  out += "".join(lines)
+  out += "---"
+
+  print(out)
+    
+  # print(json.dumps(outJson, ensure_ascii=False))
