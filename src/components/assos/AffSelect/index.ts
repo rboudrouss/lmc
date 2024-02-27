55f875a79b230389cbe3eef8805b4a9d55ccd8fa
rboudrouss <r.boudrouss@gmail.com>
2024-02-27 15:44:34 +0100

    celsa selection working

--- src/components/assos/AffSelect/index.ts
+++ src/components/assos/AffSelect/index.ts
@@ -1,3 +1,8 @@
+import CelsaS from "./CelsaS.astro";
 import LettresS from "./LettresS.astro";
+import SciencesS from "./SciencesS.astro";
+import SuS from "./SuS.astro";
+import PolyS from "./PolyS.astro";
+import SanteS from "./SanteS.astro";
 
-export { LettresS };
+export { LettresS, CelsaS, SciencesS, SuS, PolyS, SanteS };
